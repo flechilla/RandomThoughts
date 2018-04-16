1fc806554f9e86eec507c61b58dca009cc1b15f5
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 17:11:53 -0400

    IMplements the Thoughts VMs

---
+++ src/RandomThoughts/Models/ThoughtViewModels/ThoughtBaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomThoughts.Models.ThoughtViewModels
+{
+    public class ThoughtBaseViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+    }
+}
