83cb94d946521fb9491b0bf833329186d9731705
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 17:11:34 -0400

    IMplements the base api controller

---
+++ src/RandomThoughts/Controllers/Api/BaseApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomThoughts.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/v1")]
+    public class BaseApiController : Controller
+    {
+        /// <summary>
+        ///     Gets or sets the Id of the current user.
+        /// </summary>
+        public string CurrentUserId { get; set; }
+         
+
+        public BaseApiController(IHttpContextAccessor httpContextAccessor)
+        {
+            CurrentUserId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    
+    }
+}
