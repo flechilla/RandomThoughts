c1e29e287d4680baa997eeb298ec08c1c66b2b74
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 16:03:32 -0400

    Implements the base of the ThoughtsController

---
+++ src/RandomThoughts/Controllers/ThoughtsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomThoughts.DataAccess.Repositories.Thoughts;
+
+namespace RandomThoughts.Controllers
+{
+    public class ThoughtsController : Controller
+    {
+        private readonly IThoughtsRepository _thoughtsRepository;
+
+        public ThoughtsController(IThoughtsRepository thoughtsRepository)
+        {
+            _thoughtsRepository = thoughtsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
