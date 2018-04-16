61d1615a0f743aec2897fc12afed010672e81b0b
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 17:18:18 -0400

    Implements configuration for automapper and the services of the DataAccess layer

---
+++ src/RandomThoughts.DataAccess/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using RandomThoughts.DataAccess.Repositories.Thoughts;
+
+namespace RandomThoughts.DataAccess.Extensions
+{
+    /// <summary>
+    ///     Contians the functionalities to add the services 
+    ///     that are implemented in the DataAcces layer.
+    /// </summary>
+    /// <remarks>
+    ///     When a service for the DI will be used, this won't
+    ///     be neccessary.
+    /// </remarks>
+    public static class ServiceCollectionExtension
+    {
+        public static void AddDataAccessServices(this IServiceCollection service)
+        {
+            service.AddScoped<IThoughtsRepository, ThoughtsRepository>();
+        }
+    }
+}
---
+++ src/RandomThoughts/Config/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Humanizer;
+using RandomThoughts.Domain;
+using RandomThoughts.Models.ThoughtViewModels;
+
+namespace RandomThoughts.Config
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            this.CreateMap<Thought, ThoughtIndexViewModel>()
+                .AfterMap((src, dest) => dest.CreateAtHumanized = src.CreatedAt.Humanize());
+        }
+    }
+}
