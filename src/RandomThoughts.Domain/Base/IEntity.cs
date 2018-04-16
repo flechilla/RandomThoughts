78a03abceaa5a45bce18d9f18095f03b66e40731
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 10:11:39 -0400

    Implements the abstracts of the base entities

---
+++ src/RandomThoughts.Domain/Base/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomThoughts.Domain.Base
+{
+    /// <summary>
+    ///     Declare the PK for all the entities of the system.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the PK</typeparam>
+    public interface IEntity<TKey>
+    {
+        TKey Id { get; set; }
+    }
+}
