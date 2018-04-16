faf121391e3aa9bd1533a5ab2f77e7b6d6d84e0f
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-16 16:00:46 -0400

    Implements the repository for the Thought entity

---
+++ src/RandomThoughts.DataAccess/Repositories/Thoughts/IThoughtsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RandomThoughts.DataAccess.Repositories.Base;
+using RandomThoughts.Domain;
+
+namespace RandomThoughts.DataAccess.Repositories.Thoughts
+{
+    /// <summary>
+    ///     Declares the functionalities for the 
+    ///     operations on the <see cref="Thought"/> entity.
+    /// </summary>
+    public interface IThoughtsRepository : IBaseRepository<Thought, int>
+    {
+    }
+}
---
+++ src/RandomThoughts.DataAccess/Repositories/Thoughts/ThoughtsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RandomThoughts.DataAccess.Repositories.Base;
+using RandomThoughts.Domain;
+
+namespace RandomThoughts.DataAccess.Repositories.Thoughts
+{
+    public class ThoughtsRepository : BaseRepository<Thought, int>, IThoughtsRepository
+    {
+        public ThoughtsRepository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
