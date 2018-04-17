01a65c180506904389c44c30c1635ef3fb5edbf5
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 15:40:47 -0400

    Implements the ThoughtHolesController and the base of the Index view

---
+++ src/RandomThoughts/Controllers/ThoughtHolesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomThoughts.Controllers
+{
+    public class ThoughtHolesController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ThoughtHolesController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+    }
+}
