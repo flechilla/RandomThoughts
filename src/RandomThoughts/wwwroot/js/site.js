1ed33b803b449e11ed1a37f2c532020ef99d88a6
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 13:12:20 -0400

    Edits the Thought via Ajax

--- src/RandomThoughts/wwwroot/js/site.js
+++ src/RandomThoughts/wwwroot/js/site.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
         let thoughtId = $(this).closest(".thought-inner-container").data("id");
         getThought(thoughtId);        
     });
+    $(".thought-edit-btn").click(function() {
+        let thoughtId = $(this).closest(".thought-inner-container").data("id");
+        displayThoughtEditModal(thoughtId);        
+    });
     $('#add-new-thought').click(function(){
         $('#thought-edit-modal').modal('show');
     });
@@ -62,6 +66,58 @@ function saveNewThought(){
     }).done(function (res) {
         console.log('res', res);      
         toastr.success("The Thought have been created!!!");     
+    }).error(function (jqXHR, textStatus, errorThrown) {
+        toastr.error('There is an error in the server, please try again :(', 'Opps');
+    });
+}
+
+function displayThoughtEditModal(thoughtId){
+    var thoughtCntSel = $('.thought-inner-container[data-id="'+thoughtId+'"]');
+    let title = thoughtCntSel.find('.thought-title').text();
+    let body = thoughtCntSel.find('.thought-body').text();
+
+    $('#thought-edit-modal .modal-body').
+    append('<input type="text" style="display: none;" name="Id" value="'+thoughtId+'">');
+
+    $('#thought-edit-modal #thought-title').val(title);
+    $('#thought-edit-modal #thought-body').val(body);
+
+    $('#save-thought-btn').text("Edit").unbind('click').
+    click(function(){
+        saveThoughtChanges(thoughtId);
+    });
+    $('#thought-edit-modal').modal('show');
+
+}
+
+function saveThoughtChanges(thoughtId){
+    let inputsSelector = $('#thought-edit-modal input');
+    var data = inputsSelector.serializeArray();
+    let hasError = false;
+    for(key in data){
+        if(data[key].value==""){
+            var outerSelector = $('input#'+data[key].name.toLowerCase()).closest('.form-group');
+            outerSelector.addClass('has-error');
+            console.error(data[key]);
+            hasError = true;
+        }
+    }
+    let dataJson = {};
+    let form = new FormData();
+    data.map(function (x) {
+        dataJson[x.name] = x.value;
+    });
+
+    console.log(data);
+    $.ajax({
+        type: "PUT",
+        data: JSON.stringify(dataJson),
+        url: apiHost + 'Thoughts/put/'+thoughtId,
+        contentType: "application/json"
+
+    }).done(function (res) {
+        console.log('res', res);      
+        toastr.success("The Thought have been modified!!!");     
     }).error(function (jqXHR, textStatus, errorThrown) {
         toastr.error('There is an error in the server, please try again :(', 'Opps');
     });
