7f4f7952a4409531f504bd873e31f19d396d8e8c
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 10:37:16 -0400

    Implements the BaseController to contains the shared functionalities of the controllers

---
+++ src/RandomThoughts/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomThoughts.Controllers
+{
+    public class BaseController : Controller
+    {
+        /// <summary>
+        ///     Gets or sets the Id of the current user.
+        /// </summary>
+        public string CurrentUserId { get; set; }
+
+        public BaseController(IHttpContextAccessor httpContextAccessor)
+        {
+            CurrentUserId = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
