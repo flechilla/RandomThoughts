60057d9e86a24f0430775ac218283d5cff372f55
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 15:51:50 -0400

    Implements the VM for the ThoughtHole entity

---
+++ src/RandomThoughts/Models/ThoughtHoleViewModels/ThoughtHoleIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomThoughts.Models.ThoughtHoleViewModels
+{
+    public class ThoughtHoleIndexViewModel : ThoughtHoleBaseViewModel
+    {
+        public int Id { get; set; }
+
+        public string CreateAtHumanized { get; set; }
+
+        public string ModifiedAtHumanized { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Views { get; set; }
+    }
+}
