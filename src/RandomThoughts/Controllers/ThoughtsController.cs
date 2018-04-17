19ab5cf67ebf9b8fd1cd045f2a93e9e5b562b5a2
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 10:38:43 -0400

    - Inherits from the base controller.
    - Get the User's thoughts based on the value of its Id
    - IMplements action to get the public Thoughts

--- src/RandomThoughts/Controllers/ThoughtsController.cs
+++ src/RandomThoughts/Controllers/ThoughtsController.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RandomThoughts.DataAccess.Repositories.Thoughts;
+using RandomThoughts.Domain;
+using RandomThoughts.Models.ThoughtViewModels;
 
 namespace RandomThoughts.Controllers
 {
-    public class ThoughtsController : Controller
+    public class ThoughtsController : BaseController
     {
         private readonly IThoughtsRepository _thoughtsRepository;
+        private readonly IMapper _mapper;
 
-        public ThoughtsController(IThoughtsRepository thoughtsRepository)
+        public ThoughtsController(IHttpContextAccessor httpContextAccessor,
+            IThoughtsRepository thoughtsRepository,
+            IMapper mapper) : base(httpContextAccessor)
         {
             _thoughtsRepository = thoughtsRepository;
+            _mapper = mapper;
         }
 
         /// <summary>
@@ -22,11 +30,22 @@ namespace RandomThoughts.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
-            var currentUserId = HttpContext.User.Identity.Name;
+            var userThoughts = _thoughtsRepository.ReadAll(thought => thought.ApplicationUserId == this.CurrentUserId).ToList();
 
-            var userThoughts = _thoughtsRepository.ReadAll(thought => thought.ApplicationUserId == currentUserId);
+            var userThoughtsVM = _mapper.Map<IEnumerable<Thought>, IEnumerable<ThoughtIndexViewModel>>(userThoughts);
 
-            return View(userThoughts);
+            return View(userThoughtsVM);
+        }
+
+        /// <summary>
+        ///     Returns all the thoughts that belongs to the current user.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult PublicThoughts()
+        {
+            var userThoughts = _thoughtsRepository.Entities.Take(20);
+
+            return View("Index", userThoughts);
         }
     }
 }
