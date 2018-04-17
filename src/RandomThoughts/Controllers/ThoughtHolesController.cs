549f9d86e0baeee933faa563b7b671cae61c850f
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 16:08:29 -0400

    Implements the actions to get the User's Holes and the public ones

--- src/RandomThoughts/Controllers/ThoughtHolesController.cs
+++ src/RandomThoughts/Controllers/ThoughtHolesController.cs
@@ -2,20 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RandomThoughts.DataAccess.Repositories.ThoughtHoles;
+using RandomThoughts.Domain;
+using RandomThoughts.Models.ThoughtHoleViewModels;
 
 namespace RandomThoughts.Controllers
 {
     public class ThoughtHolesController : BaseController
     {
-        public IActionResult Index()
+        private readonly IThoughtHolesRepository _thoughtHolesRepository;
+        private readonly IMapper _mapper;
+
+        public ThoughtHolesController(IHttpContextAccessor httpContextAccessor,
+            IThoughtHolesRepository thoughtHolesRepository,
+            IMapper mapper) : base(httpContextAccessor)
         {
-            return View();
+            _thoughtHolesRepository = thoughtHolesRepository;
+            _mapper = mapper;
         }
 
-        public ThoughtHolesController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        [HttpGet]
+        public IActionResult Index()
         {
+            ViewData["Title"] = "Public Thoughts";
+            var thoughtHoles = _thoughtHolesRepository.ReadAll(_ => true).Select(th=>new ThoughtHole{Name = th.Name, Description = th.Description.Substring(0, 50) + "..."}).ToList();
+            var thoughtHolesVM = _mapper.Map<IEnumerable<ThoughtHole>, IEnumerable<ThoughtHoleIndexViewModel>>(thoughtHoles);
+            return View(thoughtHolesVM);
         }
+
+        [HttpGet]
+        public IActionResult MyHoles()
+        {
+            ViewData["Title"] = "Public Thoughts";
+            var thoughtHoles = _thoughtHolesRepository.ReadAll(th => th.CreatedBy == this.CurrentUserId).Select(th => new ThoughtHole { Name = th.Name, Description = th.Description.Substring(0, 50) + "..." }).ToList();
+            var thoughtHolesVM = _mapper.Map<IEnumerable<ThoughtHole>, IEnumerable<ThoughtHoleIndexViewModel>>(thoughtHoles);
+            return View("Index" ,thoughtHolesVM);
+        }
+
+
     }
 }
