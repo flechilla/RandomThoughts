14d2098672a1ee3d9996a7fad8857206be48043a
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-17 12:49:46 -0400

    Creates new Thoughts using ajax

--- src/RandomThoughts/wwwroot/js/site.js
+++ src/RandomThoughts/wwwroot/js/site.js
@@ -9,18 +9,60 @@ $(document).ready(function(){
         let thoughtId = $(this).closest(".thought-inner-container").data("id");
         getThought(thoughtId);        
     });
+    $('#add-new-thought').click(function(){
+        $('#thought-edit-modal').modal('show');
+    });
+    $('#save-thought-btn').click(saveNewThought);
 });
 
-
+/**
+ * Get the Thought data from the server.
+ * @param  {number} thoughtId The Id of the selected Thought
+ */
 function getThought(thoughtId){
     $.get(apiHost+'thoughts/get/'+thoughtId, function(data){
         displayThoughtDetails(data);
     });
 }
-
+/**
+ * TODO: Add the dates to the modal.
+ */
 function displayThoughtDetails(data){
     $('#thought-display-modal .modal-title').html(data.title);
     $('#thought-display-modal .modal-body > p').html(data.body);
     $('#thought-display-modal').modal('show');
 
+}
+
+function saveNewThought(){
+    let inputsSelector = $('#thought-edit-modal input');
+    var data = inputsSelector.serializeArray();
+    let hasError = false;
+    for(key in data){
+        if(data[key].value==""){
+            var outerSelector = $('input#'+data[key].name.toLowerCase()).closest('.form-group');
+            outerSelector.addClass('has-error');
+            console.error(data[key]);
+            hasError = true;
+        }
+    }
+    let dataJson = {};
+    let form = new FormData();
+    data.map(function (x) {
+        dataJson[x.name] = x.value;
+    });
+
+    console.log(data);
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(dataJson),
+        url: apiHost + 'Thoughts/post',
+        contentType: "application/json"
+
+    }).done(function (res) {
+        console.log('res', res);      
+        toastr.success("The Thought have been created!!!");     
+    }).error(function (jqXHR, textStatus, errorThrown) {
+        toastr.error('There is an error in the server, please try again :(', 'Opps');
+    });
 }
