9e2afd94bca10041e7ca809fb416ab4059160718
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-23 17:05:01 -0400

    Implements a foo test and adds some comments
    NOTE:
    This comments are about what the other guys have to study
    and a TODO to remember to implement a library that will have common
    functionalities for all the test projects

--- tests/RandomThoughts.Business.Tests/UnitTest1.cs
+++ tests/RandomThoughts.Business.Tests/UnitTest1.cs
@@ -3,12 +3,16 @@ using Xunit;
 
 namespace RandomThoughts.Business.Tests
 {
+    ///TODO: Study about how to use the different methods inside the Assert class. These methods are
+    ///used for different purposes
+    /// TODO: Have to implement a common library that will contain functionalities that will be used for all the tests projects.
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
+            var x = 4;
+            Assert.Equal(4, x);
         }
     }
 }
