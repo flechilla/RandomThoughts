deb0def3f367256b04a24e860e607ff49f7605ee
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-04-25 12:39:34 -0400

    add ThoughtCommentApplicationService

---
+++ src/RandomThoughts.Business/ApplicationServices/Comments/ThoughtsComments/IThoughtsCommentsApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using RandomThoughts.Business.ApplicationServices.Comments.Base;
+using RandomThoughts.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomThoughts.Business.ApplicationServices.Comments.ThoughtsComments
+{
+    public interface IThoughtsCommentsApplicationService : ICommentApplicationService<Thought, int>
+    {
+    }
+}
---
+++ src/RandomThoughts.Business/ApplicationServices/Comments/ThoughtsComments/ThoughtCommentApplicationService.cs
@@ -0,0 +1,16 @@
+﻿using RandomThoughts.Business.Base;
+using RandomThoughts.DataAccess.Repositories.Base;
+using RandomThoughts.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomThoughts.Business.ApplicationServices.Comments.ThoughtsComments
+{
+    public class ThoughtCommentApplicationService : BaseApplicationService<Comment<Thought, int>, int>, IThoughtsCommentsApplicationService
+    {
+        protected ThoughtCommentApplicationService(IBaseRepository<Comment<Thought, int>, int> repository) : base(repository)
+        {
+        }
+    }
+}
