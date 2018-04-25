7c1c2aa94010b05a542e7d87380f04ff4ad0face
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-04-25 11:59:31 -0400

    add CommentsRepository

---
+++ src/RandomThoughts.DataAccess/Repositories/Comments/CommentsRepository.cs
@@ -0,0 +1,18 @@
+﻿using RandomThoughts.DataAccess.Contexts;
+using RandomThoughts.DataAccess.Repositories.Base;
+using RandomThoughts.Domain;
+using RandomThoughts.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomThoughts.DataAccess.Repositories.Comments
+{
+    public class CommentsRepository<TEntity, TKey> : BaseRepository<Comment<TEntity, TKey>, int>,
+                                                     ICommentsRepository<TEntity, TKey> where TEntity : IEntity<TKey>
+    {
+        public CommentsRepository(RandomThoughtsDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
