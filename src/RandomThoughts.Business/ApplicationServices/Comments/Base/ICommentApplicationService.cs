129a8726f48d999fdcfad7aa44356a85071ee8ec
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-04-25 12:39:17 -0400

    add Generic CommentApplicationService

---
+++ src/RandomThoughts.Business/ApplicationServices/Comments/Base/ICommentApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using RandomThoughts.Business.Base;
+using RandomThoughts.Domain;
+using RandomThoughts.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomThoughts.Business.ApplicationServices.Comments.Base
+{
+    public interface ICommentApplicationService<TEntity, TKey> : IBaseApplicationService<Comment<TEntity, TKey>, int> where TEntity:IEntity<TKey>
+    {
+    }
+}
