fc66895b0c4ae4bcff8ef1e215b7076a929a4fc2
abelpenton <2235penton@gmail.com>
2018-04-26 14:18:38 -0700

    add ReadAll in Comments repository

--- src/RandomThoughts.DataAccess/Repositories/Comments/CommentsRepository.cs
+++ src/RandomThoughts.DataAccess/Repositories/Comments/CommentsRepository.cs
@@ -4,6 +4,9 @@ using RandomThoughts.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using RandomThoughts.Domain.Enums;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RandomThoughts.DataAccess.Repositories.Comments
 {
@@ -12,6 +15,24 @@ namespace RandomThoughts.DataAccess.Repositories.Comments
         public CommentsRepository(RandomThoughtsDbContext dbContext) : base(dbContext)
         {
 
+        }
+        /// <summary>
+        /// method for get all comments related with discriminator
+        /// TODO: use Dapper
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public IQueryable<Domain.Comments> ReadAll((int idparent, Discriminator discriminator) filter)
+        {
+            return this.Entities.Where(ent => ent.Id == filter.idparent && ent.ParentDiscriminator == filter.discriminator);            
+        }
+
+        public async Task<IQueryable<Domain.Comments>> ReadAllAsync((int idparent, Discriminator discriminator) filter)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return this.Entities.Where(ent => ent.ParentId == filter.idparent && ent.ParentDiscriminator == filter.discriminator);
+            });
         }
     }
 }
--- src/RandomThoughts.DataAccess/Repositories/Comments/ICommentsRepository.cs
+++ src/RandomThoughts.DataAccess/Repositories/Comments/ICommentsRepository.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using RandomThoughts.DataAccess.Repositories.Base;
 using RandomThoughts.Domain;
+using System.Linq;
+using RandomThoughts.Domain.Enums;
+using System.Threading.Tasks;
 
 namespace RandomThoughts.DataAccess.Repositories.Comments
 {
@@ -12,6 +15,22 @@ namespace RandomThoughts.DataAccess.Repositories.Comments
     /// </summary>
     public interface ICommentsRepository : IBaseRepository<RandomThoughts.Domain.Comments, int>
     {
-        
+        #region Sync Members
+        /// <summary>
+        ///     Filter the comments in the table based on
+        ///     the given parentID and Discriminator
+        /// </summary>
+        IQueryable<RandomThoughts.Domain.Comments> ReadAll((int idparent, Discriminator discriminator) filter);
+        #endregion
+
+        #region Async Members
+        /// <summary>
+        ///     Asynchronously filter the elements in the table based on
+        ///     the given predicate
+        /// </summary>
+        /// <param name="filter">A function to be applied in each element of the table</param>
+        /// <returns>The elements that satisfy the predicate <paramref name="filter"/></returns>
+        Task<IQueryable<RandomThoughts.Domain.Comments>> ReadAllAsync((int idparent, Discriminator discriminator) filter);
+        #endregion
     }
 }
