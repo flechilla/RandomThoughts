e154b46144c1b8aa65b437d3f8ef52eef06104cd
Adriano Flechilla <a.flechilla91@gmail.com>
2018-04-26 14:29:10 -0400

    Implement the seed for the user of the app

---
+++ src/RandomThoughts.DataAccess/Seeds/UserSeeds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using RandomThoughts.DataAccess.Contexts;
+using RandomThoughts.Domain;
+using SeedEngine;
+using Microsoft.AspNetCore.Identity;
+
+namespace RandomThoughts.DataAccess.Seeds
+{
+    /// <inheritdoc />
+    /// Contains the implementation of the seeds for 
+    /// the <see cref="ApplicationUser"/>
+    public class UserSeeds : ISeed<RandomThoughtsDbContext>
+    {
+        public void AddOrUpdate(RandomThoughtsDbContext context, int amountOfObjects = 20)
+        {
+            var mainUser = new ApplicationUser()
+            {
+                Email = "admin@gmail.com",
+                EmailConfirmed = true, 
+                NickName = "admin",
+                UserName = "admin"
+            };
+
+            var passHasher = new PasswordHasher<ApplicationUser>();
+
+            var hashedPass = passHasher.HashPassword(mainUser, "admin123");
+
+            mainUser.PasswordHash = hashedPass;
+
+            context.Users.Add(mainUser);
+
+            context.SaveChanges();
+        }
+    }
+}
