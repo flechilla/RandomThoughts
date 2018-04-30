6a3ff8442dec2e1f139b409a3b838ba22b9172a3
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-04-30 12:55:57 -0400

    commit after migration AddVisibility_Thought_ThoughtHole

--- src/RandomThoughts.DataAccess/Migrations/20180430164740_AddVisibility_Thought_ThoughtHole.Designer.cs
+++ src/RandomThoughts.DataAccess/Migrations/20180430164740_AddVisibility_Thought_ThoughtHole.Designer.cs
@@ -14,9 +14,10 @@ using RandomThoughts.Domain.Enums;
 namespace RandomThoughts.DataAccess.Migrations
 {
     [DbContext(typeof(RandomThoughtsDbContext))]
-    partial class RandomThoughtsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180430164740_AddVisibility_Thought_ThoughtHole")]
+    partial class AddVisibility_Thought_ThoughtHole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
