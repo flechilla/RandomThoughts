20b194e0ee26728630c29e3dd9e72271272e0c58
wajiro91 <a.flechilla91@gmail.com>
2019-09-16 14:39:01 -0400

    Put the shared.module in a new folder to avoid collision with the app.module

--- src/RandomThoughts/RTClientApp/src/app/shared/shared.module.ts
+++ src/RandomThoughts/RTClientApp/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { NgModule } from "@angular/core";
 import { MatInputModule, MatFormFieldModule, MatSidenavModule } from '@angular/material';
-import { AppRoutingModule } from './app-routing.module.tns';
+import { AppRoutingModule } from '../app-routing.module.tns';
 
 @NgModule({
   imports: [
