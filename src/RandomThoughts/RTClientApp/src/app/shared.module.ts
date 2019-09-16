7b966ed912bea36a5b8339ff053fa5a08c53db5b
wajiro91 <a.flechilla91@gmail.com>
2019-09-16 14:25:17 -0400

    Implement the shared module
    Now this module contains the imports from Material
    and later will include more dependencies

---
+++ src/RandomThoughts/RTClientApp/src/app/shared.module.ts
@@ -0,0 +1,25 @@
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { NgModule } from "@angular/core";
+import { MatInputModule, MatFormFieldModule, MatSidenavModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module.tns';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    AppRoutingModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    AppRoutingModule,
+  ]
+})
+export class SharedModule {}
