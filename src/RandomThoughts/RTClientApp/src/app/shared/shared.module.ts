b16b101ee3c90d4cffaff7aba8b50de4ae7a866d
wajiro91 <a.flechilla91@gmail.com>
2019-09-18 15:28:44 -0400

    Remove the NativeScript dependencies and files
    Was having some issues compiling the app,
    so I removing it. Gonna add it later

--- src/RandomThoughts/RTClientApp/src/app/shared/shared.module.ts
+++ src/RandomThoughts/RTClientApp/src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { NgModule } from "@angular/core";
 import { MatInputModule, MatFormFieldModule, MatSidenavModule } from '@angular/material';
-import { AppRoutingModule } from '../app-routing.module.tns';
 
 @NgModule({
   imports: [
@@ -11,7 +10,6 @@ import { AppRoutingModule } from '../app-routing.module.tns';
     MatInputModule,
     MatFormFieldModule,
     MatSidenavModule,
-    AppRoutingModule,
   ],
   exports: [
     MatButtonModule,
@@ -19,7 +17,6 @@ import { AppRoutingModule } from '../app-routing.module.tns';
     MatInputModule,
     MatFormFieldModule,
     MatSidenavModule,
-    AppRoutingModule,
   ]
 })
 export class SharedModule {}
