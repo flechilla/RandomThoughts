8b647c81bd48ae4999eae2a9b36a44ab13446a94
wajiro91 <a.flechilla91@gmail.com>
2019-09-19 14:40:18 -0400

    Add the urls for the ThoughtHole controller

--- src/RandomThoughts/RTClientApp/src/app/common/urls.ts
+++ src/RandomThoughts/RTClientApp/src/app/common/urls.ts
@@ -1,8 +1,20 @@
 // This file contains the definition of the URls
 
 
-export const AppDomainUrl = 'http://localhost:5001';
+export const AppDomainUrl = 'http://localhost:5001/api/v1/';
 
 // Security
 export const GetXsrfTokenUrl = `${AppDomainUrl}/security/AntiForgeryToken`;
 export const XsrfPostUrl = `${AppDomainUrl}/values`;
+
+//Thought Holes
+export const GetAllPublicHoles = `${AppDomainUrl}/ThoughtHoles/GetAllPublic`;
+export const GetAllPersonalHoles = `${AppDomainUrl}/ThoughtHoles/GetAllPersonalHoles`;
+// Needs and Id in the last segment of the URL
+export const GetHoleById = `${AppDomainUrl}/ThoughtHoles/Get`;
+export const PostHole = `${AppDomainUrl}/ThoughtHoles/Post`;
+// Needs and Id in the last segment of the URL
+export const PutHole = `${AppDomainUrl}/ThoughtHoles/Put`;
+// Needs and Id in the last segment of the URL
+export const DeleteHole = `${AppDomainUrl}/ThoughtHoles/Delete`;
+
