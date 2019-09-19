84229905f0b789f045a00f2158661abe25e55cc5
wajiro91 <a.flechilla91@gmail.com>
2019-09-19 15:54:16 -0400

    Minor change

--- src/RandomThoughts/RTClientApp/src/app/thought-hole/thought-hole.service.ts
+++ src/RandomThoughts/RTClientApp/src/app/thought-hole/thought-hole.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+/**
+ * This hole contains the functions for the communication
+ * with the server to handle the ThoughtHole data.
+ * 
+ * Also it's used for the business logic of this data.
+ *
+ * @export
+ * @class ThoughtHoleService
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ThoughtHoleService {
 
-  constructor() { }
+  constructor(httpClient: HttpClient) { }
 }
